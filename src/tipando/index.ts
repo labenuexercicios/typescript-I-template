3423c171f72a3cb707984483c2ee3df7279ac605
Mascariep <edipohc@gmail.com>
2023-01-03 11:26:38 -0300

    Prática guiada

--- src/tipando/index.ts
+++ src/tipando/index.ts
@@ -1,4 +1,19 @@
-function buscarCarrosPorMarca(frota, marca) {
+//A função ao lado recebe um array de carros e uma marca. 
+//Ela devolve os carros desta marca ou o array inteiro, caso a marca não seja passada.
+//Faça corretamente a tipagem do arquivo frota.ts, que contém os dados dos carros (marca, modelo, ano)
+//Importe os dados desse arquivo para o index.ts e faça a tipagem correta da função
+//Torne o parâmetro marca opcional
+
+
+
+
+
+import { frota } from "./frota"
+import { TCarro } from "./types"
+
+
+
+function buscarCarrosPorMarca(frota: TCarro[], marca?: string): Array<TCarro> {
   if (marca === undefined) {
     return frota
   }
@@ -9,3 +24,5 @@ function buscarCarrosPorMarca(frota, marca) {
     }
   )
 }
+
+console.log(buscarCarrosPorMarca(frota))
